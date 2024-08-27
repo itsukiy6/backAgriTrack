eba203d70ab38594e89ff25b97b4430277c838d7
Yassine <itsukigaliyak@gmail.com>
2024-08-27 12:51:55 +0100

    changementMDP

--- src/Controller/AgriculteursController.php
+++ src/Controller/AgriculteursController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\Agriculteurs;
 use App\Repository\AgriculteursRepository;
 use Doctrine\ORM\EntityManagerInterface;
-
+use JMS\Serializer\SerializationContext;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,9 +13,11 @@ use Symfony\Component\Routing\Attribute\Route;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\AgriculteurPasswordHasherInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class AgriculteursController extends AbstractController
 {
@@ -68,11 +70,41 @@ class AgriculteursController extends AbstractController
         $em->persist($agriculteur);
         $em->flush();
 
-        $jsonAgriculteur = $serializer->serialize($agriculteur, 'json', ['groups'=>'getAgriculteurs']);
 
-        $location = $urlGenerator->generate('detailAgriculteur',['id'=>$agriculteur->getId()], urlGeneratorInterface::ABSOLUTE_URL);
+        $context = SerializationContext::create()->setGroups(['getAgriculteurs']);
+        $context = $request->toArray(); 
+        $jsonAgriculteur = $serializer->serialize($agriculteur ,'json', $context);
+        return new JsonResponse($jsonAgriculteur, Response::HTTP_ACCEPTED,[], true); // Reponse json
+    }
 
-        return new JsonResponse($jsonAgriculteur, Response::HTTP_CREATED, ["Location"=>$location], true);
+
+    #[Route('/agriculteurs/{id}', name:'modifAgriculteur', methods:['PUT'])]
+    public function modifierAgriculteur(int $id, AgriculteursRepository $repo, EntityManagerInterface $em, Agriculteurs $currentAgriculteur, Request $request, SerializerInterface $serializer, ValidatorInterface $validator ): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $newAgriculteur = $serializer->deserialize($request->getContent(), Agriculteurs::class,'json');
+        $currentAgriculteur->setEmail($newAgriculteur->getEmail()); // Définit la nouvel adresse e-mail
+
+       $currentAgriculteur->setPassword(password_hash($data['password'], PASSWORD_BCRYPT));
+        $currentAgriculteur->setTel($newAgriculteur->getTel()); // Définit le nouveau numéro de téléphone
+        $errors = $validator->validate($currentAgriculteur); // Initialise l'erreur
+        if (count($errors) > 0)  // Boucle si l'Agriculteur n'existe pas dans la base de données
+        {   
+            return new JsonResponse($serializer->serialize($errors, 'json'), Response::HTTP_BAD_REQUEST, [], true);
+        } 
+        $content = $request->toArray();  // Transformer la requête en tableau
+        
+        $em->persist($currentAgriculteur); // persister 
+        $em->flush(); // enregistrer les modif
+        
+        $context = SerializationContext::create()->setGroups(['getAgriculteurs']);
+        $context = $request->toArray(); 
+        $jsonAgriculteur = $serializer->serialize($newAgriculteur ,'json', $context);
+        return new JsonResponse($jsonAgriculteur, Response::HTTP_OK,[], true); // Reponse json
+        // $context = SerializationContext::create()->setGroups(['getAgriculteurs']);
+        // $context = $request->toArray(); 
+        // $jsonNewAgriculteur = $serializer->serialize($newAgriculteur ,'json', $context);
+        // return new JsonResponse($jsonNewAgriculteur, Response::HTTP_ACCEPTED,[], true); // Reponse json
     }
 
 }
